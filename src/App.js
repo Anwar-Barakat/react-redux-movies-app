552c990dc8975c739aa183e26d178ea36228b534
Anwar-Barakat <brkatanwar0@gmail.com>
2023-10-30 13:17:58 +0300

    built the redux store. added homeSlice file.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { fetchDataFromAPI } from "./utils/api";
+
 function App() {
+  const apiTest = () => {
+    fetchDataFromAPI("/movie/popular").then((result) => console.log(result));
+  };
+
+  useEffect(() => {
+    apiTest();
+  }, []);
+
   return <div className="App">App</div>;
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,24 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.themoviedb.org/3/";
-const TMDB_TOKEN = process.env.APP_TMDB_TOKEN;
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = process.env.REACT_APP_TMDB_TOKEN;
 
 const headers = {
-  Authorization: "bearer " + TMDB_TOKEN,
+  Authorization: `Bearer ${TMDB_TOKEN}`,
+  accept: "application/json",
 };
 
 export const fetchDataFromAPI = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
+      method: "GET",
       headers,
       params,
     });
     return data;
   } catch (error) {
-    console.log(error);
+    console.log(TMDB_TOKEN);
+    console.log(process.env.API_URL);
     return error;
   }
 };
