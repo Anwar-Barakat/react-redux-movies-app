1ddf342fe9cabe9ea7ab9c7da0df46386c1bc506
Anwar-Barakat <brkatanwar0@gmail.com>
2023-11-01 18:53:58 +0300

    added official videos section to the detail page

--- src/pages/details/Detail.jsx
+++ src/pages/details/Detail.jsx
@@ -4,6 +4,7 @@ import DetailBanner from "./detailBanner/DetailBanner";
 import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Cast from "./cast/Cast";
+import Video from "./videos/Video";
 const Detail = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -16,6 +17,7 @@ const Detail = () => {
     <section>
       <DetailBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
+      <Video data={data} loading={loading} />
     </section>
   );
 };
